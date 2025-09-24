c6b2fd0eac1d2ecf72e86d99b2e7cbc8f8e97db4
GuiDSLeopoldino <guilherme.leopoldino@gmail.com>
2025-09-24 15:18:00 -0300

    criado um repositorio no github

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,110 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Tarefa> tarefas = new ArrayList<Tarefa>();
+        int proximoId = 1;
+        boolean rodando = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (rodando) {
+            System.out.println("\n===== ToDoLIST =====");
+            System.out.println("1) Adicionar tarefa");
+            System.out.println("2) Listar tarefas");
+            System.out.println("3) Marcar tarefa como concluída");
+            System.out.println("4) Desmarcar tarefa");
+            System.out.println("5) Remover tarefa");
+            System.out.println("6) Sair");
+            System.out.print("Escolha uma opção: ");
+            String op = sc.nextLine();
+
+            if (op.equals("1")) {
+                System.out.print("Digite o título da tarefa: ");
+                String titulo = sc.nextLine();
+
+                if (titulo.equals("")) {
+                    System.out.println("Título não pode ser vazio!");
+                } else {
+                    Tarefa t = new Tarefa(proximoId, titulo);
+                    tarefas.add(t);
+                    System.out.println("Tarefa adicionada com id " + proximoId);
+                    proximoId = proximoId + 1;
+                }
+
+            } else if (op.equals("2")) {
+                if (tarefas.size() == 0) {
+                    System.out.println("Nenhuma tarefa cadastrada.");
+                } else {
+                    for (int i = 0; i < tarefas.size(); i++) {
+                        Tarefa t = tarefas.get(i);
+                        String status = "[ ]";
+                        if (t.concluida == true) {
+                            status = "[X]";
+                        }
+                        System.out.println(status + " " + t.id + " - " + t.titulo);
+                    }
+                }
+
+            } else if (op.equals("3")) {
+                System.out.print("Digite o id da tarefa para concluir: ");
+                int id = sc.nextInt();
+                sc.nextLine(); // limpar buffer
+                boolean achou = false;
+                for (int i = 0; i < tarefas.size(); i++) {
+                    Tarefa t = tarefas.get(i);
+                    if (t.id == id) {
+                        t.concluida = true;
+                        System.out.println("Tarefa concluída!");
+                        achou = true;
+                    }
+                }
+                if (achou == false) {
+                    System.out.println("Id não encontrado.");
+                }
+
+            } else if (op.equals("4")) {
+                System.out.print("Digite o id da tarefa para desmarcar: ");
+                int id = sc.nextInt();
+                sc.nextLine(); // limpar buffer
+                boolean achou = false;
+                for (int i = 0; i < tarefas.size(); i++) {
+                    Tarefa t = tarefas.get(i);
+                    if (t.id == id) {
+                        t.concluida = false;
+                        System.out.println("Tarefa desmarcada!");
+                        achou = true;
+                    }
+                }
+                if (achou == false) {
+                    System.out.println("Id não encontrado.");
+                }
+
+            } else if (op.equals("5")) {
+                System.out.print("Digite o id da tarefa para remover: ");
+                int id = sc.nextInt();
+                sc.nextLine(); // limpar buffer
+                boolean removida = false;
+                for (int i = 0; i < tarefas.size(); i++) {
+                    Tarefa t = tarefas.get(i);
+                    if (t.id == id) {
+                        tarefas.remove(i);
+                        System.out.println("Tarefa removida!");
+                        removida = true;
+                        i = tarefas.size(); // força sair do loop
+                    }
+                }
+                if (removida == false) {
+                    System.out.println("Id não encontrado.");
+                }
+
+            } else if (op.equals("6")) {
+                System.out.println("Saindo...");
+                rodando = false;
+
+            } else {
+                System.out.println("Opção inválida.");
+            }
         }
     }
 }
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Tarefa {
+    public int id;
+    public String titulo;
+    public boolean concluida;
+
+    public Tarefa(int id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.concluida = false;
+    }
 }
